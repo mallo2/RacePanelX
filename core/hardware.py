ed1119d23d6e21b45604c183ddf111d63ed3039a
mallobcd <m2bc2d.pro@gmail.com>
2026-08-04 21:52:01 +0200

    delete python from mobile

--- core/hardware.py
+++
@@ -1,9 +0,0 @@
-class CoolLEDX:
-
-    @staticmethod
-    def cmdbyte_image() -> int:
-        return 0x03
-
-    @staticmethod
-    def cmdbyte_mode() -> int:
-        return 0x06
--- pro/lap_time_converter.py
+++
@@ -1,6 +0,0 @@
-def format_time(ms):
-    minutes = ms // 60000
-    seconds = (ms % 60000) // 1000
-    milliseconds = ms % 1000
-
-    return f"{minutes:02d}:{seconds:02d}:{milliseconds:03d}"
